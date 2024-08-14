ad6c1dc185475db872635a3c2cb5cf4d585a0379
Nika <v.mezerina@gmail.com>
2024-08-14 17:02:38 +0300

    -

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"net/http"
+	"x2/cmd/handlers"
 
-	"github.com/Ron-ttt/x2/cmd/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	r := mux.NewRouter()
-	r.HandleFunc("/api/user/register", handlers.Register()).Methods(http.MethodPost)
-	r.HandleFunc("/api/user/login", handlers.Login()).Methods(http.MethodPost)
-	r.HandleFunc("/api/user/orders", handlers.UpOrder()).Methods(http.MethodPost)
-	r.HandleFunc("/api/user/orders", handlers.GetOrder()).Methods(http.MethodGet)
-	r.HandleFunc("/api/user/balance", handlers.Balance()).Methods(http.MethodGet)
-	r.HandleFunc("/api/user/balance/withdraw", handlers.LossBonus()).Methods(http.MethodPost)
-	r.HandleFunc("/api/user/withdrawals", handlers.Info()).Methods(http.MethodGet)
-	r.HandleFunc("/api/orders/{number}", handlers.InfoBonus()).Methods(http.MethodGet)
+	r.HandleFunc("/api/user/register", handlers.Register).Methods(http.MethodPost)
+	r.HandleFunc("/api/user/login", handlers.Login).Methods(http.MethodPost)
+	r.HandleFunc("/api/user/orders", handlers.UpOrder).Methods(http.MethodPost)
+	r.HandleFunc("/api/user/orders", handlers.GetOrder).Methods(http.MethodGet)
+	r.HandleFunc("/api/user/balance", handlers.Balance).Methods(http.MethodGet)
+	r.HandleFunc("/api/user/balance/withdraw", handlers.LossBonus).Methods(http.MethodPost)
+	r.HandleFunc("/api/user/withdrawals", handlers.Info).Methods(http.MethodGet)
+	r.HandleFunc("/api/orders/{number}", handlers.InfoBonus).Methods(http.MethodGet)
 
-	err := http.ListenAndServe("http://localhost:8080", r)
+	err := http.ListenAndServe("localhost:666", r)
 
 	if err != nil {
 		panic(err)
