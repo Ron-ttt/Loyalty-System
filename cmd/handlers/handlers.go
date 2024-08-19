ccb165195f6aa288123d5b35de66d4d10c32d1ac
Nika <v.mezerina@gmail.com>
2024-08-19 13:32:45 +0300

    -

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -86,6 +86,7 @@ func (st start) GetOrder(res http.ResponseWriter, req *http.Request) {
 		res.WriteHeader(http.StatusUnauthorized)
 		return
 	}
+
 }
 
 func (st start) Balance(res http.ResponseWriter, req *http.Request) {
